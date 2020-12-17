ae6abb1343421b3ea1d278816c053a4679b65b41
Henry1098 <h.nzinga@codeur.online>
2020-12-17 15:53:11 +0100

    Premier commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Notre super chat !</title>
+  <link rel="stylesheet" href="app.css">
+</head>
+<body>
+  <header>
+  </header>
+  
+  <section class="chat">
+    <div class="messages">
+     
+    </div>
+    <div class="user-inputs">
+      <form action="handler.php?task=write" method="POST">
+        <input type="text" name="author" id="author" placeholder="Nickname ?">
+        <input type="text" id="content" name="content" placeholder="Type in your message right here bro !">
+        <button type="submit">🔥 Send !</button>
+      </form>
+    </div>
+  </section>
+  <script src="app.js"></script>
+</body>
+</html>
